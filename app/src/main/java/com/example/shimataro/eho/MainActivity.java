9063ec1aee4d2f81336aab1364d6558a022532ff
shimataro <shimataro@zelkova.cc>
2016-03-16 09:09:02 +0900

    * 1画面のみでリアルタイムに恵方を表示

--- app/src/main/java/com/example/shimataro/eho/MainActivity.java
+++ app/src/main/java/com/example/shimataro/eho/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.shimataro.eho;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AlertDialog.Builder m_dialog = null;
+    // 恵方
+    private enum EHO {
+        WSW, SSE, NNW, ENE,
+    }
+
+    private TextView m_textViewEho = null;
 
 
     @Override
@@ -22,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this._initActionBar();
-        this._initYearText();
-        this._initButton();
+        m_textViewEho = (TextView)findViewById(R.id.textView_eho);
+        _initActionBar();
+        _initInputYear();
     }
 
 
@@ -33,50 +34,79 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    private void _initYearText() {
+    private void _initInputYear() {
         NumberPicker numberPicker = (NumberPicker) findViewById(R.id.year);
-        numberPicker.setMinValue(0);
+
+        // 最小値・最大値を設定（西暦1年〜9999年）
+        numberPicker.setMinValue(1);
         numberPicker.setMaxValue(9999);
 
         // 今年を入れておく
-        Calendar cal = Calendar.getInstance();
-        final int year = cal.get(Calendar.YEAR);
+        final int year = _getThisYear();
         numberPicker.setValue(year);
-    }
 
-    private void _initButton() {
-        Button button = (Button)findViewById(R.id.button_calc_eho);
-        button.setOnClickListener(new View.OnClickListener() {
+        // 今年の恵方を表示
+        _setResult(year);
+
+        // 年が変わったら恵方も変える
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onClick(View v) {
-                // 確認ダイアログ
-                MainActivity.this.getDialog().create().show();
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                MainActivity.this._setResult(newVal);
             }
         });
     }
 
-    public AlertDialog.Builder getDialog() {
-        if (m_dialog == null) {
-            m_dialog = new AlertDialog.Builder(this)
-                    .setCancelable(false)
-                    .setTitle(R.string.app_name)
-                    .setMessage(R.string.eho_confirm)
-                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // 年を取得
-                            NumberPicker numberPicker = (NumberPicker)findViewById(R.id.year);
-                            final int year = numberPicker.getValue();
+    private void _setResult(final int year) {
+        final EHO eho = _getEho(year);
+        m_textViewEho.setText(_eho2str(eho));
+    }
 
-                            // 結果表示アクティビティーへ遷移
-                            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                            intent.putExtra("year", year);
-                            startActivity(intent);
-                        }
-                    });
+
+    /**
+     * 今年を取得
+     * @return 今年
+     */
+    private int _getThisYear() {
+        Calendar cal = Calendar.getInstance();
+        return cal.get(Calendar.YEAR);
+    }
+
+    /**
+     * 恵方を取得
+     * @param year 年
+     * @return 恵方（のリソースID）
+     */
+    private EHO _getEho(final int year) {
+        final EHO dirs[] = {EHO.WSW, EHO.SSE, EHO.NNW, EHO.SSE, EHO.ENE};
+        return dirs[year % 5];
+    }
+
+    /**
+     * 恵方から文字列を取得
+     * @param eho 恵方
+     * @return 恵方の文字列
+     */
+    private String _eho2str(final EHO eho) {
+        int str_id = R.string.unknown;
+        switch(eho) {
+            case WSW:
+                str_id = R.string.dir_wsw;
+                break;
+
+            case SSE:
+                str_id = R.string.dir_sse;
+                break;
+
+            case NNW:
+                str_id = R.string.dir_nnw;
+                break;
+
+            case ENE:
+                str_id = R.string.dir_ene;
+                break;
         }
-
-        return m_dialog;
+        return getString(str_id);
     }
 
 }
