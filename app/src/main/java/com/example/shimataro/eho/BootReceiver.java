66e819ac5e12626f548a357b09bcb7850b67b313
shimataro <shimataro@zelkova.cc>
2016-04-14 09:57:56 +0900

    * 参考URLを修正

--- app/src/main/java/com/example/shimataro/eho/BootReceiver.java
+++ app/src/main/java/com/example/shimataro/eho/BootReceiver.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.util.Log;
 
 /**
- * @see <a href="http://techbooster.jpn.org/andriod/application/1100/">システムの起動時にサービスを実行する «  Tech Booster</a>
+ * @see <a href="http://techbooster.org/andriod/application/1100/">システムの起動時にサービスを実行する «  Tech Booster</a>
  * @see <a href="http://qiita.com/amay077/items/e740015c00a6e7d45f6b">システムの起動時にアプリを起動する - Qiita</a>
- * @see <a href="http://blog.justoneplanet.info/2012/05/27/androidで本体の電源を入れた時にアプリを起動させる/">Androidで本体の電源を入れた時にアプリを起動させる - @blog.justoneplanet.info</a>
+ * @see <a href="http://blog.justoneplanet.info/2012/05/27/android%E3%81%A7%E6%9C%AC%E4%BD%93%E3%81%AE%E9%9B%BB%E6%BA%90%E3%82%92%E5%85%A5%E3%82%8C%E3%81%9F%E6%99%82%E3%81%AB%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92%E8%B5%B7%E5%8B%95%E3%81%95%E3%81%9B%E3%82%8B/">Androidで本体の電源を入れた時にアプリを起動させる - @blog.justoneplanet.info</a>
  * Created by odashima on 16/04/13.
  */
 public class BootReceiver extends BroadcastReceiver {
